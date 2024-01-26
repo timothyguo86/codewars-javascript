d661a79c10b46fe36d94939ba202c0bdbf5d9ba3
Timothy Guo <timothyguo86@gmail.com>
2024-01-25 19:59:25 -0500

    (feat) Created a helper function to rename the file names

---
+++ utility/renameFiles.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+const path = require('path')
+
+function renameFiles(directoryPath) {
+  // Read the list of files in the directory
+  fs.readdir(directoryPath, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err)
+      return
+    }
+
+    // Iterate through each file
+    files.forEach(filename => {
+      console.log(filename)
+      const oldFilePath = path.join(directoryPath, filename)
+
+      // Replace underscores in the filename
+      const newFilename = filename.replace(/_/g, ' ')
+
+      const newFilePath = path.join(directoryPath, newFilename)
+
+      // Rename the file
+      fs.rename(oldFilePath, newFilePath, err => {
+        if (err) {
+          console.error(`Error renaming file ${filename}:`, err)
+        } else {
+          console.log(`Renamed: ${filename} to ${newFilename}`)
+        }
+      })
+    })
+  })
+}
+
+const directoryPath = './'
+
+renameFiles(directoryPath)
