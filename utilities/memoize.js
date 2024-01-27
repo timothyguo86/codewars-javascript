4b9e2f2b7f931046e64d044ff2d8ef32cb893f6a
Timothy Guo <timothyguo86@gmail.com>
2024-01-27 18:55:56 -0500

    (doc) Add comments

--- utilities/memoize.js
+++ utilities/memoize.js
@@ -1,16 +1,17 @@
+// Memoize function to cache results
 function memoize(fn) {
+  // Cache to store results
   const cache = {}
 
+  // Return a memoized version of the function
   return function (...args) {
-    if (cache[args]) {
-      return cache[args]
-    }
+    if (cache[args]) return cache[args] // Return cached result
 
-    const result = fn.apply(this, args)
+    const result = fn.apply(this, args) // Compute result
 
-    cache[args] = result
+    cache[args] = result // Cache result
 
-    return result
+    return result // Return computed result
   }
 }
 
